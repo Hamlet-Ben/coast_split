9316f1235a844e762a10ce1e4525f8975d218fe9
Hamlet-Ben <bholt@therockpoolproject.co.uk>
2024-06-29 15:57:23 +0100

    removing need for rgdal package - no longer available

--- splitting the british coast.R
+++ splitting the british coast.R
@@ -10,6 +10,7 @@ if (!dir.exists("line parts")) {
  dir.create("line parts")
 }
 
+
 for(i in 0:14){
   
   temp <- st_linesubstring(British_coast, i/15 ,(i+1)/15)
@@ -18,23 +19,21 @@ for(i in 0:14){
   
 }
 
-#combine parts into a single polyline shapefile - I only know how to do this in rgdal so I'm loading them back in using this package
-#there may well be a more simple way to do this
+#combine parts into a single polyline shapefile - not sure why there's a need to save files and reload them in but this works
 
-
-library(rgdal)
 
 line_part_files <- list.files("line parts", pattern = ".shp") #create a vector with all the shapefile names
 
 #load
 
-res <- list() #list to save output to
+res <- list()
 
 for (file in line_part_files) {
   
-  temp <-  readOGR(paste0("line parts/", file)) #read in file
+  temp <-  st_read(paste0("line parts/", file)) #read in file
 
-  temp@data <- data.frame(ID = gsub(".*?([0-9]+).*", "\\1", file), km = SpatialLinesLengths(temp)/1000) #add data frame with ID and section length
+  temp$ID <- gsub(".*?([0-9]+).*", "\\1", file) #add ID
+  temp$km <- as.numeric(st_length(temp))/1000 #add section length in km
   
   res[[file]] <- temp #save to output list
     
@@ -42,19 +41,15 @@ for (file in line_part_files) {
 
 final_res <- do.call("rbind", res) #combine into single GIS object
 
-final_res <- final_res[order(as.numeric(final_res@data$ID)),] #get sections in correct order
- 
-rownames(final_res@data) <- 1:nrow(final_res) #fix rownames of associated data frame
-
 #plot finished GIS object
 
 plot(final_res, col = 1:nrow(final_res))
 
-writeOGR(final_res, getwd(),"Britain Coast sections.shp", driver="ESRI Shapefile", overwrite_layer = T) #export
+st_write(final_res,  "Britain Coast sections.shp", delete_layer = T)
 
 # Shapefile reprojection - in cae WGS 1984 projection is required
-final_res1984 <- spTransform(final_res, CRS("+proj=longlat +datum=WGS84"))
+final_res1984 <- st_transform(final_res, CRS("+proj=longlat +datum=WGS84"))
 
-writeOGR(final_res1984, getwd(),"Britain Coast sections WGS 1984.shp", driver="ESRI Shapefile", overwrite_layer = T) #export
+st_write(final_res1984,  "Britain Coast sections WGS 1984.shp", delete_layer = T)
 
 
